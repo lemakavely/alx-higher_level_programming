eb0bfd22e9b5e08141215e1c965ce2bc729c4ae5
lemakavely <lemakavely@gmail.com>
2022-09-30 14:26:43 -0400

    python more data structures task 1 trial 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def search_and_replace(list_to_search, find, replace):
+def search_replace(list_to_search, find, replace):
     def search_element(element):
         return (element if element != find else replace)
     return list(map(search_element, list_to_search))

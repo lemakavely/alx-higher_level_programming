2e7b361caeb65086bf541976179c0dcd5f05fc4e
lemakavely <lemakavely@gmail.com>
2022-10-12 14:55:29 -0400

    python 6-python_exceptions all tasks another trial 1

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -7,7 +7,7 @@ def magic_calculation(a, b):
                 raise Exception('Too far')
             else:
                 result += (a ** b) / j
-        except:
+        except: Exception
             result = b + a
             break
     return result

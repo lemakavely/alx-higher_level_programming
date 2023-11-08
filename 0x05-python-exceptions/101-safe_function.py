c36fd71fe81aca31e07d4d522540da247e5b4366
mohammed adem <81468057+lemakavely@users.noreply.github.com>
2023-11-08 17:28:01 +0300

    Delete 0x05-python-exceptions directory

--- 0x05-python-exceptions/101-safe_function.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def safe_function(fct, *args):
-    try:
-        res = fct(*args)
-        return res
-    except Exception as error:
-        import sys
-        print("Exception: {}".format(error), file=sys.stderr)
-        return None
--- 0x05-python-exceptions/README.md
+++
@@ -1,1 +0,0 @@
-#this is the directory of **python exceptions** projecs
